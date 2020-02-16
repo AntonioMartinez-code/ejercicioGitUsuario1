afeefd1678d01950e1cf471d53fb1aed0a7e5c92
antonio <antonio.martinezpe@elorrieta-errekamari.com>
2020-02-16 18:20:19 +0100

    commit

--- src/ejercicioGitUsusario1/mainGit.java
+++ src/ejercicioGitUsusario1/mainGit.java
@@ -4,7 +4,7 @@ public class mainGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Primer commit");
+System.out.println("Primer Commit");
 	}
 
 }
